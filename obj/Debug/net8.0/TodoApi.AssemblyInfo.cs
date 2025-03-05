6a121acec0606083a227bdc404a76ab9583936b8
Romuald <romualdgildas6@gmail.com>
2025-03-05 11:20:00 +0100

    fin du l'api Todo

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+977d4051e4bcb2af81346bba144e4ab1f0cd4435")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d3be459306f8d25b2a15751b5e9dfa9bf610c8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
